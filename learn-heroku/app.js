b1281083a4ea84a264d2ee6499cc6276ae2ef27a
sigit211 <s.wahyudi21@gmail.com>
2021-11-15 16:41:34 +0700

    update app.js

--- learn-heroku/app.js
+++ learn-heroku/app.js
@@ -1,13 +1,13 @@
-const http = require('http');
+const express = require("express")
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const app = express()
 
-server.listen(port, () => {
-  console.log(`Server running at http://:${port}/`);
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
